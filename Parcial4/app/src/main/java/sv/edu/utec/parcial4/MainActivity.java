3750bb062bfed041a48ca2341709e0df3d1072b6
LissyRosales <124642657+LissyRosales@users.noreply.github.com>
2023-05-17 21:53:42 -0600

    Parcial4

--- Parcial4/app/src/main/java/sv/edu/utec/parcial4/MainActivity.java
+++ Parcial4/app/src/main/java/sv/edu/utec/parcial4/MainActivity.java
@@ -3,12 +3,16 @@ package sv.edu.utec.parcial4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+EditText edtNomClien, edtApeClien, edtDirecccion, edtCiudad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
--- Parcial4/app/src/main/java/sv/edu/utec/parcial4/base/Clientes.java
+++ Parcial4/app/src/main/java/sv/edu/utec/parcial4/base/Clientes.java
@@ -1,5 +1,7 @@
 package sv.edu.utec.parcial4.base;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.Nullable;
 
@@ -15,11 +17,22 @@ public class Clientes extends BaseDatos{
     public long insertClientes(String NOMCLIENTE, String APELLCLIENTE, String DIRECCLIENTE, String CIUDCLIENTE){
     long codigo=0;
     try{
+        BaseDatos baseDatos= new BaseDatos(context);
+        SQLiteDatabase bd=baseDatos.getWritableDatabase();
 
+        ContentValues valoresProv = new ContentValues();
+        valoresProv.put("NOMCLIENTE", NOMCLIENTE);
+        valoresProv.put("APELLCLIENTE", APELLCLIENTE);
+        valoresProv.put("DIRECCLIENTE", DIRECCLIENTE);
+        valoresProv.put("CIUDCLIENTE", CIUDCLIENTE);
+
+        codigo=bd.insert(NOMBRE_TABLAcl, null, valoresProv);
+        return codigo;
 
         } catch (Exception e) {
-        throw new RuntimeException(e);
+        e.toString();
+        return codigo=0;
     }
-        return codigo;
+
     }
 }
